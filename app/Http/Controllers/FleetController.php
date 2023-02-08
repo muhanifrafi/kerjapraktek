6727bc39692e539c2ee74b666645b334985cd1a1
M12GUN <emghangithoeb@gmail.com>
2023-02-08 10:39:18 +0700

    update kurang db

--- app/Http/Controllers/FleetController.php
+++ app/Http/Controllers/FleetController.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class FleetController extends Controller
@@ -13,6 +14,10 @@ class FleetController extends Controller
      */
     public function index(Request $request)
     {
+        $nama = "Status";
+        $background = "header3.jpg";
+        $titles = array("Status","Fleet");
+
         return view('layouts.status.fleet');
     }
 
