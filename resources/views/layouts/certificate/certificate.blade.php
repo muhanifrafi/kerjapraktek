8bb46bf7052f9d19f19bcd9b152f4cf606af9efb
M12GUN <emghangithoeb@gmail.com>
2023-02-08 10:46:01 +0700

    raw layout

--- resources/views/layouts/certificate/certificate.blade.php
+++ resources/views/layouts/certificate/certificate.blade.php
@@ -1,59 +1,30 @@
 <!DOCTYPE html>
-<html lang="zxx" dir="ltr">
+<html lang="zxx" dir='ltr'>
+@include('includes.head')
+@include('includes.topbar')
+<!-- ======= Header ======= -->
+@include('includes.navbar')
+@include('includes.banner')
+@include('includes.breadcrumb')
+<section >
+            <div class="container">
+              <div class="row pad-row">
+                <div class="col-md-12  col-sm-12">
 
-<head>
-    <meta charset="utf-8">
-    <meta content="width=device-width, initial-scale=1.0" name="viewport">
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
-    <meta name="Author" content="IT Center PTDI" />
-    <meta name="Owner" content="PT Dirgantara Indonesia" />
-    <meta name="keywords"
-        content="customer relation management, customer, support, dirgantara, relation, indonesia, CS, PT. DI" />
-    <meta name="description" content="Customer Support Dirgantara Indonesia" />
-    <title>CUSTOMER RELATION MANAGEMENT DIRGANTARA INDONESIA</title>
-
-    <title>@yield('title')</title>
-
-    @include('includes.style')
-</head>
-
-<body>
-    <!-- ======= Top Bar ======= -->
-    <div id="topbar" class="d-none d-xl-flex align-items-center align-items-end">
-        <div class="container d-flex justify-content-end">
-            <div class="social-links">
-                <a href="{{ route('home') }}" class="login"><i class="fa fa-home"></i> Home</a>
-                <a href="gallery" class="login"><i class="fa fa-image"></i> Gallery</a>
-                <a href="footer" class="login"><i class="fa fa-address-book"></i> Contact Us</a>
-
-                <!-- Button trigger modal -->
-                <?php
-                    // if (isset($_SESSION['userid'])) {
-                    // $company = isset($_SESSION["company"]) ? $_SESSION["company"] : '';
-                ?>
-
-                {{-- <a href="logout.php"><i class="fa fa-sign-out"></i> Log Out</a> --}}
-                <?php
-                //} else {
-                ?>
-                <a href="#" class="login" data-toggle="modal" data-target="#exampleModalCenter"><i
-                        class="fa fa-sign-in"></i> Login</a>
-                <!-- <a href="#" class="login" data-toggle="modal" data-target="#frgtpasswd"><i class="fa fa-sign-in"></i> Lupa Password</a>-->
-
-                <?php //   } ?>
+				
+			<!-- PUT CONTENT HERE-->	
+            @foreach 
+                <h2></h2>
+            <br><br/>
+                
+            @endforeach
+                </div>
+			  </div>
             </div>
-        </div>
-    </div>
-    
-<div class="first">
-    <img src="http://pre12.deviantart.net/91c4/th/pre/i/2012/064/9/a/uzumaki_naruto_vs_uchiha_sasuke_by_xabracadabra-d4rsna5.jpg">
-</div>
+         </section>
 
-    <!-- BEGIN: Main Menu-->
-    @include('includes.navbar')
-    <!-- END: Main Menu-->
+  
+  </section>
+  </main>	
 
-
-    <!-- BEGIN: Main Menu-->
-    @include('includes.footer')
-    <!-- END: Main Menu-->
+@include('includes.footer')
