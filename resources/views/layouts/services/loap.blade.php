4990f68f84102fec3995c3f1814b0bfb52912a8b
rizalagatha <rizalagatha1403@gmail.com>
2023-02-10 14:25:59 +0700

    loap index

---
+++ resources/views/layouts/services/loap.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="zxx" dir='ltr'>
+@include('includes.head')
+@include('includes.topbar')
+<!-- ======= Header ======= -->
+@include('includes.navbar')
+@include('includes.banner')
+@include('includes.breadcrumb')
+<section >
+            <div class="container">
+              <div class="row pad-row">
+                <div class="col-md-12  col-sm-12">
+
+				
+			<!-- PUT CONTENT HERE-->	
+            @foreach 
+                <h2></h2>
+            <br><br/>
+                
+            @endforeach
+                </div>
+			  </div>
+            </div>
+         </section>
+
+  
+  </section>
+  </main>	
+
+@include('includes.footer')
