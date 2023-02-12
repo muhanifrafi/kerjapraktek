460db9a88b774d67b164ca878308701f838559c2
M12GUN <emghangithoeb@gmail.com>
2023-02-12 10:02:12 +0700

    Update loap.blade.php

--- resources/views/layouts/services/loap.blade.php
+++ resources/views/layouts/services/loap.blade.php
@@ -6,6 +6,22 @@
 @include('includes.navbar')
 @include('includes.banner')
 @include('includes.breadcrumb')
+
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.0/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://cdn.datatables.net/1.13.2/css/dataTables.bootstrap5.min.css">
+<link rel="stylesheet" type="text/css" media="screen" href="screen" href="main.css">
+<link href="https://fonts.googleapis.com/css?family=Cabin+Sketch|Cutive+Mono|Sue+Ellen+Francisco" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Cabin+Sketch" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Cabin+Sketch|Sue+Ellen+Francisco" rel="stylesheet">
+
+
+<script  src="https://code.jquery.com/jquery-3.5.1.js"></script>
+<script  src="https://cdn.datatables.net/1.13.2/js/jquery.dataTables.min.js"></script>
+<script  src="https://cdn.datatables.net/1.13.2/js/dataTables.bootstrap5.min.js"></script>
+
+
+
 <section >
             <div class="container">
               <div class="row pad-row">
@@ -13,18 +29,65 @@
 
 				
 			<!-- PUT CONTENT HERE-->	
-            @foreach 
-                <h2></h2>
-            <br><br/>
-                
-            @endforeach
-                </div>
-			  </div>
-            </div>
+<table id="example" class="table table-striped" style="width:100%">
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Position</th>
+                <th>Office</th>
+                <th>Age</th>
+                <th>Start date</th>
+                <th>Salary</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Tiger Nixon</td>
+                <td>System Architect</td>
+                <td>Edinburgh</td>
+                <td>61</td>
+                <td>2011-04-25</td>
+                <td>$320,800</td>
+            </tr>
+            <tr>
+                <td>Garrett Winters</td>
+                <td>Accountant</td>
+                <td>Tokyo</td>
+                <td>63</td>
+                <td>2011-07-25</td>
+                <td>$170,750</td>
+            </tr>
+            <tr>
+                <td>Ashton Cox</td>
+                <td>Junior Technical Author</td>
+                <td>San Francisco</td>
+                <td>66</td>
+                <td>2009-01-12</td>
+                <td>$86,000</td>
+            </tr>
+</tbody>
+<tfoot>
+  <tr>
+                <th>Name</th>
+                <th>Position</th>
+                <th>Office</th>
+                <th>Age</th>
+                <th>Start date</th>
+                <th>Salary</th>
+</tr>
+
+</tfoot>
+</table>
          </section>
 
   
   </section>
   </main>	
 
+<script>
+$(document).ready(function () {
+    $('#example').DataTable(
+    );
+});
+</script>
 @include('includes.footer')
