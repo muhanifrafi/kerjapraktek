076676fa29d6660b636d8ba6bd97a7574bf363d3
M12GUN <emghangithoeb@gmail.com>
2023-02-13 10:07:23 +0700

    Update certificate.blade.php

--- resources/views/layouts/certificate/certificate.blade.php
+++ resources/views/layouts/certificate/certificate.blade.php
@@ -6,25 +6,66 @@
 @include('includes.navbar')
 @include('includes.banner')
 @include('includes.breadcrumb')
-<section >
-            <div class="container">
-              <div class="row pad-row">
-                <div class="col-md-12  col-sm-12">
 
-				
-			<!-- PUT CONTENT HERE-->	
-            @foreach 
-                <h2></h2>
-            <br><br/>
-                
-            @endforeach
-                </div>
-			  </div>
-            </div>
-         </section>
+<body>
 
-  
-  </section>
-  </main>	
+<br>
+<div class="container" data-aos="fade-up">
+<div class="about-content aos-init aos-animate" data-aos="fade-left" data-aos-delay="100">
+          <h2>Certificates</h2>
+</div>
+<br>
+<div class="row">
+      <br>
+        <div class="col-md-4 d-flex align-items-stretch mb-4" data-aos="zoom-in" data-aos-delay="100">
+          <div class="icon-box">
+            <a href="certificate_detail-17-design-organization-approval-certificate">
+              <div class="icon"><img src="https://asset-a.grid.id/crop/0x0:0x0/700x0/photo/2023/02/10/052jpg-20230210011443.jpg" alt=""></div>
+              <h4>Design Organization Approval Certificate</h4>
+            </a>
+          </div>
+        </div>
+    
+	      <br>
+        <div class="col-md-4 d-flex align-items-stretch mb-4" data-aos="zoom-in" data-aos-delay="100">
+          <div class="icon-box">
+            <a href="certificate_detail-24-operating-certificate">
+              <div class="icon"><img src="frontend\assets\img\certificates\certificate_24.jpg" alt=""></div>
+              <h4>Operating Certificate</h4>
+            </a>
+          </div>
+        </div>
+    
+	      <br>
+        <div class="col-md-4 d-flex align-items-stretch mb-4" data-aos="zoom-in" data-aos-delay="100">
+          <div class="icon-box">
+            <a href="certificate_detail-26-certificate-of-approved-military-aircraft-repair-station">
+              <div class="icon"><img src="frontend\assets\img\certificates\certificate_25.jpg" alt=""></div>
+              <h4>Certificate of Approved Military Aircraft Repair Station</h4>
+            </a>
+          </div>
+        </div>
+    
+	      <br>
+        <div class="col-md-4 d-flex align-items-stretch mb-4" data-aos="zoom-in" data-aos-delay="100">
+          <div class="icon-box">
+            <a href="certificate_detail-30-bell---authorized-maintenance-center">
+              <div class="icon"><img src="frontend\assets\img\certificates\certificate_27.jpg" alt=""></div>
+              <h4>BELL - Authorized Maintenance Center</h4>
+            </a>
+          </div>
+        </div>
+    
+	      <br>
+        <div class="col-md-4 d-flex align-items-stretch mb-4" data-aos="zoom-in" data-aos-delay="100">
+          <div class="icon-box">
+            <a href="certificate_detail-32-approved-maintenance-organozation">
+              <div class="icon"><img src="https://asset-a.grid.id/crop/0x0:0x0/700x0/photo/2023/02/10/052jpg-20230210011443.jpg" alt=""></div>
+              <h4>Approved Maintenance Organozation</h4>
+            </a>
+          </div>
+        </div>
+
+</body>
 
 @include('includes.footer')
