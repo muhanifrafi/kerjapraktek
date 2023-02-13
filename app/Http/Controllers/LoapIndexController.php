a73b27b8c00105616fc60a3bfc76fcbadba7e48b
rizalagatha <rizalagatha1403@gmail.com>
2023-02-13 11:58:34 +0700

    id 1

--- app/Http/Controllers/LoapIndexController.php
+++ app/Http/Controllers/LoapIndexController.php
@@ -12,7 +12,7 @@ class LoapIndexController extends Controller
     $nama = "Service";
     $background = "header3.jpg";
     $titles = array("Services","Manuals","Aircraft Manuals","Loap Index");
-    $loap = DB::table('trloap')->where('id',2)->get();
+    $loap = DB::table('trloap')->where('id',1)->get();
     return view('layouts.services.loap',compact('loap','nama','background','titles'));
     }
 }
