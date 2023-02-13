5a3d60b8f727ce9835b5b7e513c79ae8013e1783
rizalagatha <rizalagatha1403@gmail.com>
2023-02-13 12:52:48 +0700

    search bar sb

--- resources/views/layouts/services/sb.blade.php
+++ resources/views/layouts/services/sb.blade.php
@@ -13,11 +13,58 @@
 
 				
 			<!-- PUT CONTENT HERE-->	
-            @foreach 
-                <h2></h2>
-            <br><br/>
-                
-            @endforeach
+      <script type='text/javascript'>
+                    $(function() {
+                        $("div#targetview").load("module/service_bulletin/service_bulletin-index.php");
+                    });
+                </script>
+
+                <h2> Service Bulletin</h2>
+                <form class="well" name="searchform">
+                    <input type="hidden" name="ac" value="">
+
+                    <div class="form-row py-4">
+                        <div class="col">
+                        <select class="form-control" name="aircraft">
+                                <option value="">Aircraft</option>
+                                <option value="NC212-220">NC212-220</option>
+                                <option value="C295">C295</option>
+                                <option value="N219">N219</option>
+                                <option value="CN235">CN235</option>
+                            </select>
+                        </div>
+                        <div class="col">
+                            <select class="form-control" name="e_categ">
+                                <option value="">Category</option>
+                                <option value="Mandatory">Mandatory</option>
+                                <option value="Recommended">Recommended</option>
+                                <option value="Optional">Optional</option>
+                            </select>
+                        </div>
+                        <div class="col">
+                            <select class="form-control" name="cat">
+                                <option value="">Search by</option>
+                                <option value="1">SB No.</option>
+                                <option value="2">Title</option>
+                                <option value="3">Effectivity</option>
+                              </select>
+                        </div>
+                        <div class="col">
+                            <div class="input-group searchBox">
+                                <input class="form-control clearable" type="text" placeholder="Key" name="s" />
+                                <div class="input-group-append">
+                                    <button class="btn btn-outline-secondary btn-lg buttonSearch" type="button" onclick="goSearchSB('module/service_bulletin/service_bulletin-index')" name="saring" value="Saring">
+                                        <i class="fa fa-search"></i>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+
+
+                <div id="targetview"></div>
+
                 </div>
 			  </div>
             </div>
